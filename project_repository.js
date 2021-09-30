a6a7d06627ef5f55e5c4b579d17fa87230f15c0e
Jaela-C <66124353+Jaela-C@users.noreply.github.com>
2021-09-30 15:44:03 -0500

    Modificación en las consultas

--- project_repository.js
+++ project_repository.js
@@ -5,25 +5,19 @@ class ProjectRepository {
 
     delete(id) {
       return this.dao.run(
-        `DELETE FROM projects WHERE id = ?`,
+        `DELETE FROM prueba WHERE id = ?`,
         [id]
       )
     }
 
     getById(id) {
       return this.dao.get(
-        `SELECT * FROM projects WHERE id = ?`,
+        `SELECT * FROM prueba WHERE id = ?`,
         [id])
     }
 
     getAll() {
       return this.dao.all(`SELECT * FROM prueba`)
-    }
-
-    getTasks(projectId) {
-      return this.dao.all(
-        `SELECT * FROM tasks WHERE projectId = ?`,
-        [projectId])
     }
   }
   
